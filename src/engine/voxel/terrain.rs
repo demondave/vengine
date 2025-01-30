91ff52d63b8d146024a6b8d7a764b7f3a813a7f9
fdai7374 <jonas.gaertner1@informatik.hs-fulda.de>
2025-01-30 14:54:58 +0100

    feat(worldgen): added basic world gen with sin and cos waves

---
+++ src/engine/voxel/terrain.rs
@@ -0,0 +1,76 @@
+use std::sync::Arc;
+
+use ahash::{HashMap, HashMapExt};
+use cgmath::{Matrix4, SquareMatrix, Vector3};
+use wgpu::Device;
+
+use crate::engine::{core::engine::Engine, renderer::pass::Pass};
+
+use super::{
+    chunk::{Chunk, CHUNK_SIZE},
+    object::ChunkEx,
+};
+
+pub struct Terrain {
+    distance: i32,
+    chunks: HashMap<Vector3<i32>, ChunkEx>,
+    device: Arc<Device>,
+}
+
+impl Terrain {
+    pub fn new(distance: i32, device: Arc<Device>) -> Terrain {
+        Terrain {
+            distance,
+            chunks: HashMap::with_capacity((distance * 2 * distance * 2) as usize),
+            device,
+        }
+    }
+
+    pub fn render(&mut self, engine: &Engine, pass: &mut Pass) {
+        let eye = engine.camera().get_eye();
+
+        let eye_x = eye.x as i32 / CHUNK_SIZE as i32;
+        let eye_z = eye.z as i32 / CHUNK_SIZE as i32;
+
+        for z in (eye_z - self.distance)..(eye_z + self.distance) {
+            for x in (eye_x - self.distance)..(eye_x + self.distance) {
+                match self.chunks.get(&Vector3::new(x, 0, z)) {
+                    Some(chunk) => {
+                        pass.render_chunk(Matrix4::identity(), Vector3::new(x, 0, z), chunk);
+                    }
+                    None => {
+                        let min_x = x * CHUNK_SIZE as i32;
+                        let min_z = z * CHUNK_SIZE as i32;
+
+                        let mut chunk = Chunk::empty();
+
+                        for z in 0..CHUNK_SIZE {
+                            for x in 0..CHUNK_SIZE {
+                                let n = heightmap(min_x + x as i32, min_z + z as i32);
+
+                                chunk.set(x, n, z, true);
+
+                                for n in 0..n {
+                                    chunk.set(x, n, z, true);
+                                }
+                            }
+                        }
+
+                        let mut chunk = ChunkEx::new(chunk);
+                        chunk.remesh();
+                        chunk.allocate(&self.device);
+
+                        pass.render_chunk(Matrix4::identity(), Vector3::new(x, 0, z), &chunk);
+
+                        self.chunks.insert(Vector3::new(x, 0, z), chunk);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn heightmap(x: i32, z: i32) -> usize {
+    (((x as f32 * 0.1).sin() * 10.0 + (z as f32 * 0.333).cos() * 15.0) as i64)
+        .clamp(0, CHUNK_SIZE as i64) as usize
+}
